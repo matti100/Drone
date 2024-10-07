57148465de39e8172d814121f13fac95a106ba22
matti100 <163460767+matti100@users.noreply.github.com>
2024-10-07 21:03:57 +0200

    webApp

---
+++ WebApp/drone-web-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="controPanel">
+        <h1>Drone Control panel</h1>
+        <form>
+          <label>Insert input commands</label>
+          <input type="number"></input>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
