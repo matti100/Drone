da9ad7549da7a5a0790e3ee9244297886c3a53df
matti100 <163460767+matti100@users.noreply.github.com>
2024-10-15 13:42:48 +0200

    simulink

--- simulator/simulink/Data.m
+++ simulator/simulink/Data.m
@@ -1,43 +1,55 @@
 %% DATA
-% Reference: Hummingbird
+
+%% Problem initialization
+params = struct();
 
 % Inertial Data:
-m = 0.5;        % [Kg]              % Mass
-    
-A = 2.32e-3;    % [Kg / m^2]        % Principal Moment of Inertia X
-B = 2.32e-3;    % [Kg / m^2]        % Principal Moment of Inertia Y
-C = 4.00e-3;    % [Kg / m^2]        % Principal Moment of Inertia Z
-I = diag([A, B, C]);                % Principal Inertia Matrix
+params.m = 1.25;            % [Kg]              % Mass
+params.m = 0.8;
+params.Ix = 0.0232;          % [Kg / m^2]        % Principal Moment of Inertia X
+params.Ix = 15.67*1e-3;
+params.Iy = 0.0232;         % [Kg / m^2]        % Principal Moment of Inertia Y
+params.Iy = 15.67*1e-3;
+params.Iz = 0.0468;         % [Kg / m^2]        % Principal Moment of Inertia Z
+params.Iz = 28.34*1e-3;
+params.I = diag([params.Ix, params.Iy, params.Iz]);
+% Principal Inertia Matrix
 
 % Geometric Data
-
-L= 0.2;        % [m]                % Arm length
+params.armLength = 0.265;   % [m]                % Arm length
+params.armLength = 0.3;
+params.bodySize = 0.2;      % [m]                % body size
+params.rotRad = 0.1;        % [m]                % rotor radius
 
 % Torque and Lift Coefficients
-k_f = 6.11e-8;  % [N / rpm]         % Lift Coefficient                  
-k_m = 1.5e-9;   % [N*m / rpm]       % Torque Coefficient
+k_f = 6.11e-8;   % [N / rpm]         % Lift Coefficient
+params.k_f = 0.02;
+params.k_f = 192.32*1e-7;
+% params.k_f = (k_f)*60/6.28;                % [N/rad/s]
+k_m = 1.5e-9;    % [N*m / rpm]       % Torque Coefficient
+params.k_m = 0.01;
+params.k_m = 4.003*1e-7;
+% params.k_m = (k_m)*60/6.28;                % [N/rad/s]
 
 % Gravitational Data
-g = -9.81;      % [m/s^2]           % gravity acceleration
+params.g = 9.81;        % [m/s^2]            % Gravity acceleration
 
+% Time interval
+params.dt = 0.01;       % [s]                % Time interval
 
-%% Save data in a structure Data type
-copter_data = struct();
+% Initial Condition
+x0 = zeros(12,1);
+x0(1) = 0;
+x0(2) = 0;
+x0(3) = 1;
+x0(7) = 0;
+x0(8) = 0;
+x0(9) = 0;
 
-% Inertial Data
-copter_data.mass = m;
-copter_data.principal_intertia_moments = [A, B, C];
-
-% Geometric Data
-copter_data.arm = L;
-
-% Dynamics Data
-copter_data.lift_coeff = k_f;
-copter_data.torque_coeff = k_m;
-copter_data.g = g;
-
-% Save the data structure in the "Variables" folder
-save('Variables/Data.mat', 'copter_data');
+% Desidered Position
+desideredState = struct();
+desideredState.rDes = [1, -0.5, 2]';      % [m]
+desideredState.attDes = [0, 0, pi/2]';
 
 
 
