025fd54c9b12fc9f5b3dc5f342249eeca70942c8
matti100 <matteo.colussi03@gmail.com>
2026-09-12 10:52:24 -0400

    Refactoring the file

--- simulator/sim_script/simulator.m
+++ simulator/sim_script/simulator.m
@@ -7,55 +7,52 @@ w = warning ('on','all');
 %% Problem initialization
 params = struct();
 
-% Inertial Data:
-params.m = 1.25;            % [Kg]              % Mass
-params.m = 0.8;
-params.Ix = 0.0232;          % [Kg / m^2]        % Principal Moment of Inertia X
-params.Ix = 15.67*1e-3;
-params.Iy = 0.0232;         % [Kg / m^2]        % Principal Moment of Inertia Y
-params.Iy = 15.67*1e-3;
-params.Iz = 0.0468;         % [Kg / m^2]        % Principal Moment of Inertia Z
-params.Iz = 28.34*1e-3;
-params.I = diag([params.Ix, params.Iy, params.Iz]);
-% Principal Inertia Matrix
+% Inertial Data:             
+params.m = 0.8;             % Mass [Kg]       
+params.Ix = 15.67*1e-3;     % Principal X Moment of Inertia [Kg / m^2]       
+params.Iy = 15.67*1e-3;     % Principal Y Moment of Inertia [Kg / m^2]
+params.Iz = 28.34*1e-3;     % Principal Z Moment of Inertia [Kg / m^2]
+params.I = diag([params.Ix, params.Iy, params.Iz]);  % Principal Inertia Matrix
 
-% Geometric Data
-params.armLength = 0.265;   % [m]                % Arm length
-params.armLength = 0.3;
-params.bodySize = 0.2;      % [m]                % body size
-params.rotRad = 0.1;        % [m]                % rotor radius
+% Geometric Data            
+params.armLength = 0.3;     % Arm length [m]   
+params.bodySize = 0.2;      % Body size [m]               
+params.rotRad = 0.1;        % Rotor radius [m]                
 
 % Torque and Lift Coefficients
-k_f = 6.11e-8;   % [N / rpm]         % Lift Coefficient
-params.k_f = 0.02;
-params.k_f = 192.32*1e-7;
-params.k_f = 1.4*1e-5 / 9.54929658;
+params.k_f = 1.4*1e-5 / 9.54929658;          % Lift Coefficient [N / rpm]
 % params.k_f = (k_f)*60/6.28;                % [N/rad/s]
-k_m = 1.5e-9;    % [N*m / rpm]       % Torque Coefficient
-params.k_m = 0.01;
-params.k_m = 4.003*1e-7;
-params.k_m = 1.78*1e-6 / 9.54929658;
+params.k_m = 1.78*1e-6 / 9.54929658;         % Torque Coefficient [N*m / rpm]
 % params.k_m = (k_m)*60/6.28;                % [N/rad/s]
 
 % Gravitational Data
-params.g = 9.81;        % [m/s^2]            % Gravity acceleration
+params.g = 9.81;                             % [m/s^2]
 
 % Time interval
-params.dt = 0.001;       % [s]                % Time interval
+params.dt = 0.001;                           % [s]
 
 % Initial Condition
 x0 = zeros(12,1);
-x0(1) = 0;
-x0(2) = 0;
-x0(3) = 0;
-x0(7) = 0.3;
-x0(8) = 0.5;
-x0(9) = 0;
+x0(1) = 0;       % Initial x position [m]
+x0(2) = 0;       % Initial y position [m]
+x0(3) = 0;       % Initial z position [m]
 
-% Desidered Position
+x0(4) = 0;       % Initial x velocity [m/s]
+x0(5) = 0;       % Initial y velocity [m/s]
+x0(6) = 0;       % Initial z velocity [m/s]
+
+x0(7) = 0.3;     % Initial roll [rad]
+x0(8) = 0.5;     % Initial pitch [rad]
+x0(9) = 0;       % Initial yaw [rad]
+
+x0(10) = 0;      % Initial roll rate [rad/s]
+x0(11) = 0;      % Initial pitch rate [rad/s]
+x0(12) = 0;      % Initial yaw rate [rad/s]
+
+% Desidered Position and Attitude
 desideredState = struct();
 desideredState.rDes = [-0.5, 1, 1]';      % [m]
-desideredState.attDes = [0, 0, 0]';
+desideredState.attDes = [0, 0, 0]';       % [rad]
 
 % Initialize simulation time
 t0 = 0;                 % [s]
@@ -75,9 +72,8 @@ estimation = 0;        % 0 -> no estimation
                        % 1 -> kalman filter
 
 % Discrete Time
-sampleTime = 0.005;      % [s] sample time for the discrete system
+sampleTime = 0.005;          % [s] sample time for the discrete system
                              % sampleTime = params.dt -> continous time
-                             % 0.01
 
 % Plot flag
 plot_flag = 1;               % 1 -> plot                0 -> no plot
@@ -141,47 +137,26 @@ else
     
     % Manual tuning                             % I set                 II set
     kP = [20;           % -> kP_T                  20                     20
-          -0.4;            % -> kP_phi                0
+          -0.4;         % -> kP_phi                0
           0.4;          % -> kP_theta              0.4
           1;            % -> kP_R                  1                      1.55
           1;            % -> kP_P                  1                      1.55
           1.2];         % -> kP_Y                  0.1                  x 0.02
 
     kI = [10;           % -> kI_T                  10                     10
-          -0.09;            % -> kI_phi                0
+          -0.09;        % -> kI_phi                0
           0.09;         % -> kI_theta              0.09
           0.13;         % -> kI_R                  0.13                   0.17
           0.13;         % -> KI_P                  0.13                   0.17
-          0.03];       % -> KI_Y                  0.013                x 0.01
+          0.03];        % -> KI_Y                  0.013                x 0.01
 
     kD = [10;           % -> kD_T                  10                     10
-          -0.25;         % -> kD_phi                0
+          -0.25;        % -> kD_phi                0
           0.25;         % -> kD_theta              0.25
           0.66;         % -> kD_R                  0.66                   0.76
           0.66;         % -> kD_P                  0.66                   0.76
-          0.9];       % -> kD_Y                  0.066                x 0.01
-
-    %     % Manual tuning                             % I set                 II set
-    % kP = [10;           % -> kP_T                  40                     20
-    %       0;            % -> kP_phi                0
-    %       0;          % -> kP_theta              0.4
-    %       0;            % -> kP_R                  1                      1.55
-    %       0;            % -> kP_P                  1                      1.55
-    %       0];         % -> kP_Y                  0.1                  x 0.02
-    % 
-    % kI = [0;           % -> kI_T                  30                     10
-    %       0;            % -> kI_phi                0
-    %       0;         % -> kI_theta              0.09
-    %       0;         % -> kI_R                  0.13                   0.17
-    %       0;         % -> KI_P                  0.13                   0.17
-    %       0];       % -> KI_Y                  0.013                x 0.01
-    % 
-    % kD = [10;           % -> kD_T                  0.2                     10
-    %       0;         % -> kD_phi                0
-    %       0;         % -> kD_theta              0.25
-    %       0;         % -> kD_R                  0.66                   0.76
-    %       0;         % -> kD_P                  0.66                   0.76
-    %       0];       % -> kD_Y                  0.066                x 0.01
+          0.9];         % -> kD_Y                  0.066                x 0.01
+          
     
     % load("tunedGains_ga.mat");
     % gains = tunedGains;
